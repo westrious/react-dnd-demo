c87d32c4b0cacfff25f3a53edc8eea69be4c6736
zhangxiangyu.777 <zhangxiangyu.777@bytedance.com>
2021-12-13 18:33:42 +0800

    textarea中错误的gid变红

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 // eslint-disable-next-line
 import { DragSource, DropTarget, DndProvider, DragSourceMonitor, DragSourceConnector, ConnectDragSource, DragSourceSpec, DropTargetSpec, DropTargetMonitor, DragSourceCollector, DropTargetCollector, DropTargetConnector, ConnectDropTarget, XYCoord } from 'react-dnd';
-import { render, findDOMNode } from 'react-dom';
+import { render } from 'react-dom';
+import { TextArea } from '@douyinfe/semi-ui';
 import picture from './const';
+
+const fontStyle = { fontSize: 14, lineHeight: '20px' };
 
 interface AppProps { }
 interface Gid2Picture {
   gid: string;
   picture: string;
+  isError: boolean;
 }
 interface AppState {
   text: string;
@@ -21,31 +25,31 @@ class App extends React.Component<AppProps, AppState> {
     this.state = {
       text: '',
       gid2Picture: [
-        { gid: '11111111111', picture },
-        { gid: '22222222222', picture },
-        { gid: '33333333333', picture },
-        { gid: '44444444444', picture },
-        { gid: '55555555555', picture },
-        { gid: '66666666666', picture },
-        { gid: '77777777777', picture },
-        { gid: '88888888888', picture },
-        { gid: '99999999999', picture },
-        { gid: '00000000000', picture },
-        { gid: '01111111111', picture },
-        { gid: '02222222222', picture },
-        { gid: '03333333333', picture },
-        { gid: '04444444444', picture },
-        { gid: '05555555555', picture },
-        { gid: '06666666666', picture },
-        { gid: '07777777777', picture },
-        { gid: '08888888888', picture },
-        { gid: '09999999999', picture },
-        { gid: '10000000000', picture },
-        { gid: '21111111111', picture },
-        { gid: '32222222222', picture },
-        { gid: '13333333333', picture },
-        { gid: '14444444444', picture },
-        { gid: '15555555555', picture },
+        { gid: '11111111111', picture, isError: false },
+        { gid: '22222222222', picture, isError: false },
+        { gid: '33333333333', picture, isError: false },
+        { gid: '44444444444', picture, isError: false },
+        { gid: '55555555555', picture, isError: true },
+        { gid: '66666666666', picture, isError: false },
+        { gid: '77777777777', picture, isError: false },
+        { gid: '88888888888', picture, isError: false },
+        { gid: '99999999999', picture, isError: false },
+        { gid: '00000000000', picture, isError: false },
+        { gid: '01111111111', picture, isError: false },
+        { gid: '02222222222', picture, isError: false },
+        { gid: '03333333333', picture, isError: false },
+        { gid: '04444444444', picture, isError: true },
+        { gid: '05555555555', picture, isError: false },
+        { gid: '06666666666', picture, isError: false },
+        { gid: '07777777777', picture, isError: false },
+        { gid: '08888888888', picture, isError: false },
+        { gid: '09999999999', picture, isError: false },
+        { gid: '10000000000', picture, isError: false },
+        { gid: '21111111111', picture, isError: false },
+        { gid: '32222222222', picture, isError: true },
+        { gid: '13333333333', picture, isError: false },
+        { gid: '14444444444', picture, isError: false },
+        { gid: '15555555555', picture, isError: false },
       ],
     };
   }
@@ -58,16 +62,21 @@ class App extends React.Component<AppProps, AppState> {
     });
   }
 
-  handleGidChange = ({ target }: React.ChangeEvent<HTMLTextAreaElement>) => {
+  handleGidChange = (text: string) => {
     this.setState({
-      text: target.value,
+      text,
     });
   };
 
   // 按回车或者失去焦点的时候，把textarea里的值传给list
   handleGidList = (value: string) => {
+    const { gid2Picture } = this.state;
+    const nowGidList = value.trim().split('\n').map(gid => {
+      const gid2PictureItem = gid2Picture.find((item) => item.gid === gid) as Gid2Picture;
+      return gid2PictureItem;
+    });
     this.setState({
-      gid2Picture: value.trim().split('\n').map((item: string) => ({ gid: item, picture })),
+      gid2Picture: nowGidList,
     });
   };
 
@@ -81,16 +90,29 @@ class App extends React.Component<AppProps, AppState> {
     this.handleGidList(target.value);
   };
 
+  renderMask = () => {
+    const { gid2Picture } = this.state;
+    return (
+      <>
+        {gid2Picture.map((item, index) => item.isError ? (<div key={index} style={{ ...fontStyle, color: 'red', pointerEvents: 'none', position: 'absolute', top: 6 + index * 20, left: 13 }}>{item.gid}</div>) : null)}
+      </>
+    );
+  }
+
   renderGid = () => {
     const { text } = this.state;
     return (
-      <textarea
-        value={text}
-        onChange={this.handleGidChange}
-        onKeyUp={this.handleGidKeyUp}
-        onBlur={this.handleGidBlur}
-        style={{ width: 200, height: 200 }}
-      />
+      <div style={{ position: 'relative', width: 200, height: 200, overflow: 'scroll' }}>
+        <TextArea
+          value={text}
+          autosize
+          onChange={this.handleGidChange}
+          onKeyUp={this.handleGidKeyUp}
+          onBlur={this.handleGidBlur as any}
+          style={{ ...fontStyle }}
+        />
+        {this.renderMask()}
+      </div>
     );
   };
 
@@ -103,7 +125,7 @@ class App extends React.Component<AppProps, AppState> {
       text: gid2Picture.map((item) => item.gid).join('\n'),
       gid2Picture,
     });
-  }
+  };
   renderPicture = () => {
     const { gid2Picture } = this.state;
     return (
@@ -117,7 +139,7 @@ class App extends React.Component<AppProps, AppState> {
           }}
         >
           {gid2Picture.map((item, index) => (
-            <Picture key={item.gid} index={index} moverItem={this.moverItem} gid={item.gid} picture={item.picture} />
+            <Picture key={item.gid} index={index} moverItem={this.moverItem} {...item} />
           ))}
         </div>
       </DndProvider>
@@ -137,6 +159,7 @@ class App extends React.Component<AppProps, AppState> {
 interface PictureProps {
   gid: string;
   picture: string;
+  isError: boolean;
   index: number;
   moverItem: (dragIndex: number, hoverIndex: number) => void;
 }
@@ -172,7 +195,7 @@ const dragSourceSpec = {
     };
   },
   endDrag(props: PictureProps, monitor: DragSourceMonitor, component: Picture) {
-    console.log('end')
+    console.log('end');
     const item: DragObject = monitor.getItem();
     const dropResult = monitor.getDropResult();
     dropResult && console.log(`gid: ${item.gid}, dropResult: ${dropResult}`);
@@ -197,18 +220,18 @@ const dropTagretSpec = {
       return;
     }
 
-    const hoverBoundingRect = (findDOMNode(component) as Element).getBoundingClientRect();
-    const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
-    const clientOffset = monitor.getClientOffset() as XYCoord;
-    const hoverClientX = clientOffset.x - hoverBoundingRect.left;
+    // const hoverBoundingRect = (findDOMNode(component) as Element).getBoundingClientRect();
+    // const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
+    // const clientOffset = monitor.getClientOffset() as XYCoord;
+    // const hoverClientX = clientOffset.x - hoverBoundingRect.left;
 
-    if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
-      return;
-    }
+    // if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+    //   return;
+    // }
 
-    if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
-      return;
-    }
+    // if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+    //   return;
+    // }
 
     monitor.getItem().index = hoverIndex;
     props.moverItem(dragIndex, hoverIndex);
@@ -233,13 +256,13 @@ const dropTargetCollector = (connect: DropTargetConnector, moniter: DropTargetMo
 class Picture extends React.Component<PictureProps & DragCollectedProps & DropCollectedProps> {
 
   render() {
-    const { picture, connectDragSource, isDragging, connectDropTarget } = this.props;
+    const { isError, picture, connectDragSource, isDragging, connectDropTarget } = this.props;
     return connectDropTarget!(connectDragSource!(
-      <div style={{ width: 54, height: 54, margin: 10, cursor: 'move', opacity: isDragging ? 0.5 : 1, backgroundColor: 'orange', display: 'inline-block', position: 'relative' }}>
+      <div style={{ width: 54, height: 54, margin: 10, cursor: 'move', opacity: isDragging ? 0.5 : 1, border: isError ? 'red 1px solid' : undefined, boxSizing: 'border-box', display: 'inline-block', position: 'relative' }}>
         <img src={picture} alt="demo" style={{ width: '100%', height: '100%', pointerEvents: 'none' }} />
         <div style={{ position: 'absolute', top: 0, right: 0, width: 10, height: 10, backgroundColor: 'blue' }}></div>
       </div>
-    ))
+    ));
   }
 }
 
